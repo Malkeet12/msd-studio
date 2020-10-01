725dba823b30067c69d14c5c5fd485742c460a73
Malkeet singh <bs001@MacBook-Pro.domain.name>
2020-10-02 02:56:12 +0530

    Updates

---
+++ precache-manifest.4024839fe0d276adf3c6358f7ed2e952.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13f3596510efbaba2bd734c3ee800a85",
+    "url": "/msd-studio/index.html"
+  },
+  {
+    "revision": "625b9a552bf635c06814",
+    "url": "/msd-studio/static/css/main.d5c36521.chunk.css"
+  },
+  {
+    "revision": "f99a8ad6b465fbe39641",
+    "url": "/msd-studio/static/js/2.d1771525.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/msd-studio/static/js/2.d1771525.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "625b9a552bf635c06814",
+    "url": "/msd-studio/static/js/main.7f820e15.chunk.js"
+  },
+  {
+    "revision": "82cec965b2a39854b5f9",
+    "url": "/msd-studio/static/js/runtime-main.b1a4d61a.js"
+  },
+  {
+    "revision": "9d39e4cbb62062297fc74504f9674165",
+    "url": "/msd-studio/static/media/digipaper.9d39e4cb.png"
+  },
+  {
+    "revision": "db9b21a1c41f3dcd9731e1e7acfdbb57",
+    "url": "/msd-studio/static/media/get_it_on_google_play.db9b21a1.png"
+  },
+  {
+    "revision": "b829176d952c53509da9c4422f9a8811",
+    "url": "/msd-studio/static/media/time_pass.b829176d.png"
+  }
+]);
