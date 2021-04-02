e74c4491321795abe94b7c934a5356e5b118ad3b
Malkeet <malkeet@bluestacks.com>
2021-04-02 23:58:41 +0530

    moving routes to separate file

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,20 @@
-import React, { useState } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
 import "styles/index.scss";
 import BurgerMenu from "./shared/burger_menu";
-import { RouterPathEnum } from "./constants/paths";
-import Home from "./components/home";
-import TimePassPrivacyPolicy from "./components/apps/time_pass";
-import AboutUs from "./components/about_us";
-import DigipaperPrivacyPolicy from "./components/apps/digipaper";
 import Footer from "components/footer";
+import Routes from "./routes";
 
 class App extends React.Component {
   render() {
     return (
-      <div className="main-app">
-        <BurgerMenu />
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
-          <Switch>
-            <Route exact={true} path={"/"} component={Home} />
-          </Switch>
-          <Switch>
-            <Route
-              exact={true}
-              path={RouterPathEnum.ABOUT_US}
-              component={AboutUs}
-            />
-          </Switch>
-          <Switch>
-            <Route exact={true} path={RouterPathEnum.HOME} component={Home} />
-          </Switch>
-          <Switch>
-            <Route
-              exact={true}
-              path={RouterPathEnum.TIME_PASS_POLICY}
-              component={TimePassPrivacyPolicy}
-            />
-          </Switch>
-          <Switch>
-            <Route
-              exact={true}
-              path={RouterPathEnum.DIGIPAPER_POLICY}
-              component={DigipaperPrivacyPolicy}
-            />
-          </Switch>
-        </BrowserRouter>
-        <Footer />
-      </div>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <div className="main-app">
+          <BurgerMenu />
+          <Routes />
+          <Footer />
+        </div>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/routes.jsx
@@ -0,0 +1,59 @@
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { RouterPathEnum } from "./constants/paths";
+import Home from "./components/home";
+import TimePassPrivacyPolicy from "./components/apps/time_pass";
+import AboutUs from "./components/about_us";
+import DigipaperPrivacyPolicy from "./components/apps/digipaper";
+import DrumKit from 'components/javascript30/drum_kit';
+
+
+class Routes extends Component {
+    render() {
+        return (
+            <Fragment>
+                <Switch>
+                    <Route exact={true} path={"/"} component={Home} />
+                </Switch>
+
+                <Switch>
+                    <Route exact={true} path={RouterPathEnum.HOME} component={Home} />
+                </Switch>
+
+                <Switch>
+                    <Route
+                        exact={true}
+                        path={RouterPathEnum.ABOUT_US}
+                        component={AboutUs}
+                    />
+                </Switch>
+
+                <Switch>
+                    <Route
+                        exact={true}
+                        path={RouterPathEnum.TIME_PASS_POLICY}
+                        component={TimePassPrivacyPolicy}
+                    />
+                </Switch>
+
+                <Switch>
+                    <Route
+                        exact={true}
+                        path={RouterPathEnum.DIGIPAPER_POLICY}
+                        component={DigipaperPrivacyPolicy}
+                    />
+                </Switch>
+
+                <Switch>
+                    <Route
+                        exact={true}
+                        path={RouterPathEnum.DRUM_KIT}
+                        component={DrumKit}
+                    />
+                </Switch>
+            </Fragment>
+        );
+    }
+}
+
+export default Routes;
