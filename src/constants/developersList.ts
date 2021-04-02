db9dd09b384be2efcfbbbbf9f827cd4c76d03db1
Malkeet <malkeet@bluestacks.com>
2021-04-02 23:59:00 +0530

    uncommited stuff

--- src/constants/developersList.ts
+++ src/constants/developersList.ts
@@ -1,12 +1,13 @@
 import malkeetDP from "images/developers/malkeet.jpg";
 import tavishiDP from "images/developers/tavishi.jpeg";
+import rahulDP from "images/developers/rahul.jpeg";
 
 export const DevelopersList: any = [
   {
     name: "Malkeet Singh",
     message: ` Malkeet sees amazing things in our future. And why not? An ardent practitioner of simple,
       relevant ideas, under his leadership, we’ve created games that have smashed titles. But
-      even more importantly, Malkeet believes the things that make msdstudio special – including
+      even more importantly, Malkeet believes the things that make msdStudio special – including
       our dedication and commitment, is humility that fuels and inspires everyone to do great
       work, and then some.
       `,
@@ -21,7 +22,22 @@ export const DevelopersList: any = [
       root in her presence.  She shows quiet intensity in the pursuit
       of getting anything right.
       `,
-    profile: `Tavishi holds a CS Degree from MNNIT Allahabad and started her career as an Fullstack developer 2 years ago.`,
+    profile: `Tavishi holds a CS Degree from MNNIT Allahabad and started her career as an Fullstack developer 2 years ago. She's architected msdStudio Scalable Server`,
     dp: tavishiDP,
+  },
+  {
+    name: "Varun Bansal",
+    message: `How do you prepare for an unprecedented change in disruptive technological advancements? 
+    That’s the task Varun has set forth for himself.  
+    he has worked on several global titles over his 5 year career span. 
+    `,
+    profile: `Outside of work, you will find him with his handy iPad designing the perfect images of uncommon birds.`,
+  },
+  {
+    name: "Rahul Chandyok",
+    message: `Rahul is responsible for overseeing msdStudio's global finance operations including accounting, investor relations, tax, as well as leading operations.
+    `,
+    profile: `With over 8 years of experience in industry, Rahul has touched nearly every area of the business.`,
+    dp: rahulDP,
   },
 ];
